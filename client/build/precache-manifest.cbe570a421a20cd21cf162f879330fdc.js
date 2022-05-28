c8218839e5d60f0f2aec5c7bff89ab2117e5bedc
prabhakarzha <kumarprabhakar141@gmail.com>
2022-05-28 08:19:31 +0530

    first commit

---
+++ client/build/precache-manifest.cbe570a421a20cd21cf162f879330fdc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d78df66f148e1d49756d32b59526094c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c498ecb2a6965c7682f3",
+    "url": "/static/css/main.217c5480.chunk.css"
+  },
+  {
+    "revision": "f33dfd3818d7db94c95a",
+    "url": "/static/js/2.2613a76d.chunk.js"
+  },
+  {
+    "revision": "603816eec26c1aff4a38bef8d08d40a6",
+    "url": "/static/js/2.2613a76d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c498ecb2a6965c7682f3",
+    "url": "/static/js/main.44825db4.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  },
+  {
+    "revision": "d0d07a3ddd2902a33a9fe45c42162359",
+    "url": "/static/media/showcase.d0d07a3d.jpg"
+  }
+]);
